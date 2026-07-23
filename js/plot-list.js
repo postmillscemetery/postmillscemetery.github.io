6608921b3f4744c340cad745734efbccc2852fc9
postmillscemetery <post.mills.cemetery@gmail.com>
2026-07-23 08:44:17 -0400

    Update plot-list.js

--- js/plot-list.js
+++ js/plot-list.js
@@ -1,5 +1,5 @@
 //window.addEventListener('json_CemeteryDirectoryReady',plotList);
-setTimeout(plotList,1000);
+setTimeout(plotList,2000);
 function plotList() {
     if (!window.json_PlotTable_6) return;
     var features = window.json_PlotTable_6.features || [];
