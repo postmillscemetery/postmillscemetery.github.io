b72c9f091eeb7427a78d8dc2246a8cfd10b301e3
postmillscemetery <post.mills.cemetery@gmail.com>
2026-07-26 18:03:10 -0400

    Update cemetery-map.js

--- js/cemetery-map.js
+++ js/cemetery-map.js
@@ -387,7 +387,7 @@
         map.getPane('pane_Plots_1').style.zIndex = 401;
         map.getPane('pane_Plots_1').style['mix-blend-mode'] = 'normal';
         //window.addEventListener('json_CemeteryDirectoryReady',plotLayer());
-        setTimeout(plotLayer,1000);
+        setTimeout(plotLayer,4000);
         function plotLayer(){
         var layer_Plots_1 = new L.geoJson(json_Plots_1, {
             attribution: '',
