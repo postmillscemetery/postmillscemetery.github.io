be595d7a7101a99cfa7c2706a6ac66a817bee932
postmillscemetery <post.mills.cemetery@gmail.com>
2026-07-30 10:57:20 -0400

    Update plot-summary.js

--- js/plot-summary.js
+++ js/plot-summary.js
@@ -1,4 +1,4 @@
-setTimeout(plotSummary,10000);
+setTimeout(plotSummary,8000);
 function plotSummary () {
     if (!window.json_PlotTable_6) return;
     var features = json_PlotTable_6.features || [];
